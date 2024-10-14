ea55edd7468899443ed97241e6b933a52ee34201
faiza shariff <kevinropy@gmail.com>
2024-10-14 07:27:01 -0700

    first commit

---
+++ areaofcircle.py
@@ -0,0 +1,4 @@
+radius=20
+pi=3.142
+area=pi*radius*radius
+print(area)
---
+++ intro.py
@@ -0,0 +1,27 @@
+# print{fname}
+#used to display the python output on the terminal 
+fname="faiza shariff"
+fname="techcamp"
+print(fname)
+#write a program that adds 2 variables num1 10 num2 20 and stores them in a variables called sum
+num1=10
+num2=20
+sum=num1+num2
+print(sum)
+
+
+#declare 2 variables fname="faiza" lname="shariff" add them up to get full name
+fname="faiza"
+lname="shariff"
+fullname=fname+lname
+print(fullname)
+fname="faiza"
+lname="shariff"
+full_name="faiza shariff"
+print(full_name)
+
+marks=input("enter your marks ?")
+print(marks) 
+#get an input of phone number on the terminal
+phone= input("enter your phone number")
+print(phone)
